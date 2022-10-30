4510e6339466b970da195392419fbf11baf4c5e7
Andrii-Dmytrenk0 <dmitrenko1567@gmail.com>
2022-10-30 12:06:04 +0100

    app 1 task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,34 @@
+const btnStart = document.querySelector('[data-start]');
+const btnStop = document.querySelector('[data-stop]');
+let colorInterval = null;
+// const btnStartExamin = btnStart.disabled = false;
+// const btnStopExamin = btnStop.disabled = false;
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+btnStart.addEventListener('click', onStart);
+btnStop.addEventListener('click', onStop);
+
+function onStart() {
+   colorInterval = setInterval(() => {
+        document.querySelector('body').style.backgroundColor = getRandomHexColor();
+   }, 1000);
+    examinationStart();
+};
+
+function onStop() {
+    clearInterval(colorInterval);
+    examinationStop();
+};
+
+function examinationStart() {
+    btnStart.disabled = true;
+    btnStop.disabled = false;
+};
+
+function examinationStop() {
+    btnStart.disabled = false;
+    btnStop.disabled = true;
+};
